96ff0c5493c17a11f74c6a9f4dae862c04ff6a95
Stephan Porz <s.porz@uni-bonn.de>
2013-03-12 17:58:10 +0100

    added bare python file

---
+++ sudoku_solver.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""sudoku_solver.py
+
+Solves Sudoku.
+
+"""
+
+__status__ = "Development"
+
+import argparse
+import numpy
+
+if __name__ == "__main__":
+    pass
